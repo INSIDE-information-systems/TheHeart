03ec5e83233cb36ad359945d66f1615de9af352d
Nicolas Havet <nhavet@etud.u-pem.fr>
2019-02-25 18:06:00 +0100

    first version of Network

---
+++ Network.py
@@ -0,0 +1,44 @@
+from network import LoRa
+import socket
+
+class Network:
+    def __init__(self, config):
+        self.data = None
+        self.lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868, sf=12, device_class = LoRa.CLASS_A)
+        app_eui = ubinascii.unhexlify(config.app_eui)
+        app_key = ubinascii.unhexlify(config.app_key)
+        self.lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+        while not self.lora.has_joined():#replace by async
+            time.sleep(2.5)
+            print('Not yet joined...')
+
+    def send(): # TODO: optimise for power efficiency
+        # create a LoRa socket
+        s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+        # set the LoRaWAN data rate
+        s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+        # make the socket blocking
+        # (waits for the data to be sent and for the 2 receive windows to expire)
+        s.setblocking(True)
+
+        # send some data
+        s.send("test4, paquet:"+str(i))#[0x01, 0x02, 0x03]))
+        time.sleep(10)
+
+        # make the socket non-blocking
+        # (because if there's no data received it will block forever...)
+        s.setblocking(False)
+
+        # get any data received (if any...)
+        self.data = s.recv(64) # TODO: set to the correct lenght
+
+    def hasMessage():
+        if self.data is None:
+            return False
+        else:
+            return True
+
+    def getMessage():
+        msg = self.data
+        self.data = None
+        return msg
