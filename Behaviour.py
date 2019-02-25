9c9257b0af3ff752dad74aa8d72b24194f0bd8e5
Nicolas Havet <nhavet@etud.u-pem.fr>
2019-02-25 17:07:04 +0100

    frequency is now global and in minutes, removed repetition of Mode

--- Behaviour.py
+++ Behaviour.py
@@ -2,18 +2,18 @@ from modes import Modes
 import machine
 
 class Behaviour:
-    def __init__(self, network, confign mode = Modes.OFF):
+    def __init__(self, network, config):
         self.network = network
         self.configuration = config
+        self.sleepTime = 1000*60*self.configuration.frequency # TODO: find a more elegant way to set the time
         if self.network.hasMessage():
             # TODO: handle reconf
             self.configuration()
         else:
             if self.mode = Modes.OFF
-                minutes = 1 # TODO: find a more elegant way to set the time
-                self.off(1000*60*minutes)# TODO: set check frequency
+                self.off()# TODO: set check frequency
             elif self.mode = Modes.PERIODIC:
-                self.periodic(config.)# TODO: get periodicity
+                self.periodic()# TODO: get periodicity
             elif self.mode = Modes.RESPONSIVE:
                 self.responsive()
             elif self.mode = Modes.PERFORMANCE:
@@ -22,23 +22,23 @@ class Behaviour:
                 self.off()# TODO: set check frequency
 
     def off(sleepTime):
-        machine.deepsleep(sleepTime)
+        machine.deepsleep(self.sleepTime)
 
     def periodic(sleepTime):
         value = sensor.getValue()
         network.send(value)
-        machine.deepsleep(sleepTime)
+        machine.deepsleep(self.sleepTime)
 
     def responsive():
         machine.pin_deepsleep_wakeup(["P13"], machine.WAKEUP_ANY_HIGH, False)# TODO: set the right pin
         value = sensor.getValue()
         network.send(value)
-        machine.deepsleep(sleepTime)
+        machine.deepsleep(self.sleepTime)
 
     def performance(sleepTime):
         while (not self.network.hasMessage()):
             value = sensor.getValue()
             network.send(value)
-            utime.sleep_ms(sleepTime)
+            utime.sleep_ms(self.sleepTime)
         # TODO: handle reconf
         self.configuration()
--- Configuration.py
+++ Configuration.py
@@ -1,10 +1,9 @@
 class Configuration:
-
     def __init__(self,id, sensorDriver,mode, freq):
         self.id = id # TODO: find a more elegant way to do that
         self.sensorDriver = sensorDriver
         self.mode = mode
-        self.freq = freq
+        self.freq = freq #time in minutes
     def applyConfiguration(jsonString):
         loadedObject = ujson.loads(jsonString)
         self.id = loadedObject[id]
