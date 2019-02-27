a9ed9b5777244a91bb5ded537f28d9c3f73f8d16
Nicolas Havet <nhavet@etud.u-pem.fr>
2019-02-27 16:27:14 +0100

    moved configuration and deepsleep

--- Behaviour.py
+++ Behaviour.py
@@ -6,36 +6,30 @@ class Behaviour:
         self.network = network
         self.configuration = config
         self.sleepTime = 1000*60*self.configuration.frequency # TODO: find a more elegant way to set the time
+
+        if self.mode = Modes.PERIODIC:
+            self.periodic()# TODO: get periodicity
+        elif self.mode = Modes.RESPONSIVE:
+            self.responsive()
+        elif self.mode = Modes.PERFORMANCE:
+            self.performance()# TODO: set check frequency
+
         if self.network.hasMessage():
             # TODO: handle reconf
             self.configuration()
-        else:
-            if self.mode = Modes.OFF
-                self.off()# TODO: set check frequency
-            elif self.mode = Modes.PERIODIC:
-                self.periodic()# TODO: get periodicity
-            elif self.mode = Modes.RESPONSIVE:
-                self.responsive()
-            elif self.mode = Modes.PERFORMANCE:
-                self.performance()# TODO: set check frequency
-            elif self.mode = Modes.UNAUTHORISED:
-                self.off()# TODO: set check frequency
 
-    def off(sleepTime):
         machine.deepsleep(self.sleepTime)
 
-    def periodic(sleepTime):
+    def periodic():
         value = sensor.getValue()
         network.send(value)
-        machine.deepsleep(self.sleepTime)
 
     def responsive():
         machine.pin_deepsleep_wakeup(["P13"], machine.WAKEUP_ANY_HIGH, False)# TODO: set the right pin
         value = sensor.getValue()
         network.send(value)
-        machine.deepsleep(self.sleepTime)
 
-    def performance(sleepTime):
+    def performance():
         while (not self.network.hasMessage()):
             value = sensor.getValue()
             network.send(value)
