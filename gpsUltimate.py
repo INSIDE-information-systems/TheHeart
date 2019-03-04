1183faa9b8082e59c47b5c793204323eed2b0fbf
ibrahima <ibrahimahotia.diallo@gmail.com>
2019-03-04 18:11:20 +0100

    mise à jour de la fonction calculDistance

--- gpsUltimate.py
+++ gpsUltimate.py
@@ -46,12 +46,11 @@ class UltimateGPS:
         date = day+"/"+month+"/"+year+" "+hour+":"+minute+":"+sec
         return date
     def calculDistance(lat1,lat2,lon1,lon2):
-        R = 6373.0
         dlon = lon2 - lon1
         dlat = lat2 - lat1
-        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        return R * c
+        if dlat>0.00001 and dlon>0.00001:
+            print("update")
+            return True
         #TODO: "Rectifier le calcul"
 
 # this uses the UART_1 default pins for TXD and RXD (``P3`` and ``P4``)
