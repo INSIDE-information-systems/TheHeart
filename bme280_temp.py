442ae7828941b3e33670ee1a8b00ef700e0a48b9
ibrahima <ibrahimahotia.diallo@gmail.com>
2019-03-04 13:57:02 +0100

    quelques modifs sur le bus i2c

--- bme280_temp.py
+++ bme280_temp.py
@@ -3,16 +3,20 @@ from bme280 import *
 from time import sleep
 
 class TemperatureSensor():
+    i2c = I2C(0, pins=('P9','P10'))     # create and use non-default PIN assignments (P9=SDA, P10=SCL)
+    i2c.init(I2C.MASTER, baudrate=115200) # init as a master
+
+    #i2c = I2C(scl=Pin(22), sda=Pin(23))
+    bme = BME280(i2c=i2c)
 
     @property
     def getValue(self):
+        i2c = I2C(0, pins=('P9','P10'))     # create and use non-default PIN assignments (P9=SDA, P10=SCL)
+        i2c.init(I2C.MASTER, baudrate=115200) # init as a master
+
+        #i2c = I2C(scl=Pin(22), sda=Pin(23))
+        bme = BME280(i2c=i2c)
         return bme.getTemperature
-
-i2c = I2C(0, pins=('P9','P10'))     # create and use non-default PIN assignments (P9=SDA, P10=SCL)
-i2c.init(I2C.MASTER, baudrate=115200) # init as a master
-
-#i2c = I2C(scl=Pin(22), sda=Pin(23))
-bme = BME280(i2c=i2c)
 
 bme280Temperature = TemperatureSensor()
 bme280Temperature.getValue
