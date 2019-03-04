da9ec9c6057e905f8ae3757fb69ef7de9866fecc
Marylene <marylene.apa@gmail.com>
2019-03-04 17:41:34 +0100

    fixed function getBattery in SensorThings

--- SensorThings.py
+++ SensorThings.py
@@ -1,42 +1,37 @@
 import ujson
 from gpsUltimate import UltimateGPS
-import Power
+from Power import Power
 import DriverManager
 
 #La classe SensorThings sert à mettre en forme un message au format json utilisable par SensorThings API
 class SensorThings :
 
-#Fonction qui prend en paramètre un objet "GPS", dans lequel on va placer la valeur des coordonnées polaires
+#Fonction qui prend en paramètre un objet "gpsUltimate", dans lequel on va placer la valeur des coordonnées polaires
 #coordonnées(latitute en °, longitude en °)
 #La fonction retourne le message de position au format json pour sensorThings API
     def sensorthingify_location(location):
         msg_location = {
-            #"coordinates" : gps
             "coordinates" : location
                 }
         return ujson.dumps(msg_location)
 
 
-#Fonction qui prend en paramètre un objet "GPS", un objet "battery" et un objet "value"
-#Dans lesquels on va placer respectivement la date et l'heure renvoyées par le module gps
-#Une estimation de la valeur de la batterie du The+Heart
-#Et enfin la valeur du capteur I2C branché
-#La fonction retourne le message de l'observation au format json pour sensorThings API
+#La fonction sensorthingify_observation met en forme un message d'observation au format json
     def sensorthingify_observation(measurement):
+        battery = Power.getBattery()
         parameters = {
-        "battery" : 10#battery.getBattery
-        #"battery" : battery
+            "battery" : battery
         }
 
         msg_observation = {
             "resultTime" : measurement.date,
-            #"resultTime" : gps,
             "result" : measurement.value,
             "parameters" : parameters
         }
-        #print(msg)
         return ujson.dumps(msg_observation)
 
+#La fonction sensorThingify renvoie les messages qu'il y a à envoyer
+#Soit que un message d'observation soit un message d'observation et un message de location
     def sensorThingify(measurement, sessionData):
         print("sensorThingify")
         msg_obs = SensorThings.sensorthingify_observation(measurement)
@@ -44,6 +39,6 @@ class SensorThings :
         updateLocation = UltimateGPS.calculDistance(measurement.location[0],sessionData.lastGpsCoordinates[0],measurement.location[1],sessionData.lastGpsCoordinates[1])
         if(updateLocation>=5):
             msg_loc = SensorThings.sensorthingify_location(measurement.location)
-            return [msg_loc,msg_obs]
+            return (msg_loc,msg_obs)
         else:
-            return [msg_obs]
+            return (msg_obs)
