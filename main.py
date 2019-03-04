35272fac087cccced060d331a8f78c9403d0954d
Nicolas Havet <nhavet@etud.u-pem.fr>
2019-03-04 12:53:36 +0100

    minor fixes

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ import SessionData
 gps = gpsUltimate.UltimateGPS() #GPS init first to save time (fixing coordinates can be quite long)
 measurement = Measurement.Measurement(gps)
 data = SessionData.SessionData() # TODO: what if there is no configuration saved
-network = Network.Network()
+network = Network.Network(data)
 Behaviour.Behaviour(network, data, measurement)
