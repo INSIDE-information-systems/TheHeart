61a1dcdf7f2890a59c047ebd493da278c92eb726
Nicolas Havet <nhavet@etud.u-pem.fr>
2019-03-04 12:58:04 +0100

    minor fixes

--- DriverManager.py
+++ DriverManager.py
@@ -1,7 +1,7 @@
-#from bme280 import *
-#from bme280_humidity import *
-#from bme280_temp import *
-#from bme280_pression import *
+from bme280 import *
+from bme280_humidity import *
+from bme280_temp import *
+from bme280_pression import *
 from pir import *
 
 class DriverManager:
