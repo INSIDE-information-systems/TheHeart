6165ff2fae1ea1662808b44bd36884ef8f1fe193
ibrahima <ibrahimahotia.diallo@gmail.com>
2019-03-05 18:27:56 +0100

    I2C bus fixed again

--- bme280_humidity.py
+++ bme280_humidity.py
@@ -3,11 +3,11 @@ from bme280 import *
 from time import sleep
 
 class HumiditySensor():
-    i2c = I2C(0, pins=('P9','P10'))     # create and use non-default PIN assignments (P9=SDA, P10=SCL)
-    i2c.init(I2C.MASTER, baudrate=115200) # init as a master
-    #i2c = I2C(scl=Pin(22), sda=Pin(23))
-    bme = BME280(i2c=i2c)
+    def __init__(self,i2c,bme):
 
+        self.i2c = I2C(0, pins=('P9','P10'))     # create and use non-default PIN assignments (P9=SDA, P10=SCL)
+        i2c.init(I2C.MASTER, baudrate=115200) # init as a master
+        self.bme = BME280(i2c=i2c)
     @property
     def getValue(self):
         i2c = I2C(0, pins=('P9','P10'))     # create and use non-default PIN assignments (P9=SDA, P10=SCL)
